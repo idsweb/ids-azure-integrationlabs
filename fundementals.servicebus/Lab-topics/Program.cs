7dc4495d98f4fdab9119bbe1b9496e68e9333efd
idsweb <iandstafford@googlemail.com>
2021-03-23 21:01:30 +0000

    removed connection string

---
+++ fundementals.servicebus/Lab-topics/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+
+namespace Lab_topics
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            ManagementClient m_ManagementClient = new ManagementClient("connectionstring");
+            string topicPath = "demotopic";
+            string subscriptionName = "orderSubscription";
+
+            // create the filter
+            var filter = new CorrelationFilter();
+            filter.Properties["State"] = "USA";
+            var rule = new RuleDescription("Demo", filter);
+
+            var subscriptionDescription = new SubscriptionDescription(topicPath, subscriptionName);
+
+            await m_ManagementClient.CreateSubscriptionAsync(subscriptionDescription, rule);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
